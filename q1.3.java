bac1c679a2f9633a528dbe627b578d889d783e58
eraserxp <eraser117xp@gmail.com>
2014-09-16 12:27:50 -0700

    add another question

---
+++ q1.3.java
@@ -0,0 +1,20 @@
+/**
+* Given two strings, write a method to decide if one is a permutation
+* of the other
+*/
+
+public class PermutationTest {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static boolean isPermutation(String s1, String s2) {
+		if (s1.length != s2.length) return false;
+		
+		//assuming it is ascii characters
+		char [] char_set = new char[256];
+		return false;
+	}	
+}		
+
