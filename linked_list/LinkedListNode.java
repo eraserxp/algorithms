0f16bf3ea32ded5d813140a1b8bbb4113254f08c
eraserxp <eraser117xp@gmail.com>
2014-09-26 17:04:29 -0700

    add implementation for linkedlist node

---
+++ linked_list/LinkedListNode.java
@@ -0,0 +1,31 @@
+
+
+public class LinkedListNode {
+	public LinkedListNode next = null;
+	public int data;
+	
+	public LinkedListNode(int d) {
+		data = d;
+	}
+	
+	
+	public void add(int d) {
+		LinkedListNode end = new LinkedListNode(d);
+		LinkedListNode n = this;
+		while (n.next != null ) {
+			n = n.next;
+		}
+		n.next = end;
+	}
+	
+	
+	public void print() {
+		LinkedListNode n = this;
+		while (n != null) {
+			System.out.printf("%d --> ", n.data);
+			n = n.next;
+		}
+
+		System.out.println(" ");	
+	}		
+}	
--- linked_list/RemoveDuplicates.java
+++ linked_list/RemoveDuplicates.java
@@ -3,3 +3,43 @@
  * Follow up
  * How would you solve this problem if a temporary buffer is not allowed
  */ 
+
+ 
+public class RemoveDuplicates {
+	
+	public static void removeDuplicatesSimple(LinkedListNode ll) {
+		while (ll.next!=null) {
+			LinkedListNode nodeForComparison = ll.next;
+			while (nodeForComparison!=null) {
+				// delete the duplicates
+				if (nodeForComparison.data == ll.data) {
+					if (nodeForComparison.next!=null) { 
+						nodeForComparison.next = nodeForComparison.next.next;
+					} else { // if it is the end of the linked list
+						nodeForComparison = null;
+						break; // jump out of the inner while loop
+					}	
+				}
+				nodeForComparison = nodeForComparison.next;	
+			}	
+			ll = ll.next;
+		}	
+
+	}		
+	
+	public static void main(String[] args) {
+		LinkedListNode ll = new LinkedListNode(3);
+		ll.add(1);
+		ll.add(2);
+		ll.add(1);
+		ll.add(3);
+		ll.add(9);
+		ll.add(2);
+		System.out.println("The original linked list is: " );
+		ll.print();
+		removeDuplicatesSimple(ll);
+		System.out.println("After deleting duplicates, the linked list is: " );
+		ll.print();
+	}	
+	
+}	
