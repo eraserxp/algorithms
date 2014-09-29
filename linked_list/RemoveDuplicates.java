b2b2c258a8ddb260dff3919066e294080d86bae4
eraserxp <eraser117xp@gmail.com>
2014-09-29 16:34:54 -0700

    finish question 2.1

--- linked_list/RemoveDuplicates.java
+++ linked_list/RemoveDuplicates.java
@@ -3,29 +3,50 @@
  * Follow up
  * How would you solve this problem if a temporary buffer is not allowed
  */ 
-
+import java.util.*;
  
 public class RemoveDuplicates {
 	
 	public static void removeDuplicatesSimple(LinkedListNode ll) {
-		while (ll.next!=null) {
-			LinkedListNode nodeForComparison = ll.next;
-			while (nodeForComparison!=null) {
+		// if the linked list is empty or has only one node
+		if (ll == null) return;
+		
+		while (ll!=null) {
+			LinkedListNode prevNode = ll;
+			LinkedListNode curNode = ll.next;
+			
+			// compare all nodes after ll with ll
+			while (curNode!=null) {
 				// delete the duplicates
-				if (nodeForComparison.data == ll.data) {
-					if (nodeForComparison.next!=null) { 
-						nodeForComparison.next = nodeForComparison.next.next;
-					} else { // if it is the end of the linked list
-						nodeForComparison = null;
-						break; // jump out of the inner while loop
-					}	
+				if (curNode.data == ll.data) {					
+					prevNode.next = curNode.next;				
 				}
-				nodeForComparison = nodeForComparison.next;	
+				prevNode = curNode;
+				curNode = curNode.next;	
 			}	
+			
 			ll = ll.next;
 		}	
 
+	}	
+	
+	public static void removeDuplicatesHash(LinkedListNode ll) {
+		HashSet<Integer> dataSet = new HashSet<Integer>();
+		// add the data into the set and check if the set already contains the data
+		LinkedListNode prevNode = null;
+		while (ll!=null) {
+			if ( dataSet.contains(ll.data) ) { // if it is a duplicate
+				prevNode.next = ll.next;
+			} else {	
+				dataSet.add(ll.data);
+				// you let prevNode = ll only if ll is not the node to be deleted
+				// otherwise, you don't need to change prevNode
+				prevNode = ll; 
+			}			
+			ll = ll.next;
+		}	
 	}		
+	
 	
 	public static void main(String[] args) {
 		LinkedListNode ll = new LinkedListNode(3);
@@ -38,6 +59,18 @@ public class RemoveDuplicates {
 		System.out.println("The original linked list is: " );
 		ll.print();
 		removeDuplicatesSimple(ll);
+		System.out.println("After deleting duplicates, the linked list is: " );
+		ll.print();
+
+		// add some duplicates
+		ll.add(2);
+		ll.add(1);
+		ll.add(3);
+		ll.add(9);
+		System.out.println("After adding some duplicates, the linked list is" );
+		ll.print();
+		//removeDuplicatesSimple(ll);
+		removeDuplicatesHash(ll);
 		System.out.println("After deleting duplicates, the linked list is: " );
 		ll.print();
 	}	
