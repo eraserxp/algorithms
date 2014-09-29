cb09c536b0d02e821bcc392fdbc69d3934b6b152
eraserxp <eraser117xp@gmail.com>
2014-09-29 16:43:25 -0700

    add question 2.2

---
+++ linked_list/FindKthToLast.java
@@ -0,0 +1,16 @@
+/*
+ * implement an algorithm to find the kth to last element of a singly 
+ * linked list
+ * 
+ */
+ 
+public class FindKthToLast {
+	
+	public static int kthToLast(LinkedListNode l) {
+		
+	}	
+	
+	public static void main() {
+		
+	}	
+}	  
